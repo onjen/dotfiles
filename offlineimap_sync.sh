20b9578dafd33ab4bc7d3131a38e8e987e82fbd1
Johannes Rothe <rothe@magazino.eu>
2018-05-19 00:02:14 +0200

    offlineimap_sync.sh: quiet mode

--- offlineimap_sync.sh
+++ offlineimap_sync.sh
@@ -4,10 +4,11 @@
 # shamelessly stolen from
 # https://hobo.house/2015/09/09/take-control-of-your-email-with-mutt-offlineimap-notmuch/
 
+source /home/rothe/.profile
 imapserver=$(cat ~/.offlineimaprc | grep remotehost | awk '{print $3}')
 imapactive=$(ps -ef | grep '[o]fflineimap' | wc -l)
 netactive=$(ping -c3 $imapserver >/dev/null 2>&1 && echo up || echo down)
-mailsync="/usr/bin/offlineimap"
+mailsync="/usr/bin/offlineimap -o -u quiet"
 
 # kill offlineimap if active, sometimes it hangs
 case $imapactive in
